0ce2c80dbfcb9b1f0efca088245ced4917adaa34
shumal-odoo <139279617+shumal-odoo@users.noreply.github.com>
2024-02-04 13:49:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// powerbi-client v2.21.1
+// powerbi-client v2.21.1.0
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 (function webpackUniversalModuleDefinition(root, factory) {
